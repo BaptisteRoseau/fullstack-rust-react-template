904294b362b7ed131b61cebd0a1c4abe872240b6
Baptiste Roseau <broseau@gmail.com>
2025-12-27 13:08:30 +0100

    Move database created_at and updated_at event trigger in database migrations

--- crates/database/migrations/0001_created_at_updated_at_trigger.down.sql
+++ crates/database/migrations/0001_created_at_updated_at_trigger.down.sql
@@ -1,1 +1,3 @@
--- Add down migration script here
+DROP EVENT TRIGGER IF EXISTS ensure_timestamps_ddl;
+DROP FUNCTION IF EXISTS add_item_creation_and_update_fields_trigger;
+DROP FUNCTION IF EXISTS update_modified_column;
