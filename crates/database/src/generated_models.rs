529aab878707892c2ee83c65afc86b6cb2779e5c
Baptiste Roseau <broseau@gmail.com>
2026-01-16 22:46:38 +0100

    Added generated_models.rs

---
+++ crates/database/src/generated_models.rs
@@ -0,0 +1,11 @@
+
+#[derive(Debug, Clone, sqlx::FromRow)]
+pub struct User {
+    id: uuid::Uuid,
+    last_name: String,
+    first_name: String,
+    email: String,
+    address: Option<String>,
+    created_at: chrono::DateTime<chrono::Utc>,
+    updated_at: chrono::DateTime<chrono::Utc>,
+}
