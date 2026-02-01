37e1ab8326cac4e240d77f240fa571175d86ce82
Baptiste Roseau <broseau@gmail.com>
2026-02-01 18:33:02 +0100

    Remove models and add basics of storage crate.

--- crates/models/src/lib.rs
+++
@@ -1,9 +0,0 @@
-pub mod auth;
-pub mod comment;
-pub mod company;
-pub mod error;
-pub mod profile;
-pub mod user;
-
-use std::collections::HashMap;
-pub type DeleteWrapper = HashMap<(), ()>;
---
+++ crates/storage/src/storage.rs
@@ -0,0 +1,6 @@
+pub trait Storage {
+    // save
+    // load
+    // save_enrypted
+    // load_encrypted
+}
