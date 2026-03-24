397d7735f4acce69e1eddcc863b111ba7f34de24
Baptiste Roseau <baptiste.roseau@opendatasoft.com>
2026-03-24 21:22:46 +0100

    Comment out mailer integration tests

--- crates/mailer/tests/integration.rs
+++ crates/mailer/tests/integration.rs
@@ -1,25 +1,25 @@
-use mailer::*;
+// use mailer::*;
 
-#[test]
-fn builder() {
-    let mut _mailer = Mailer::new(
-        "smtp.server.com",
-        "smtp_username",
-        "smtp_password",
-        Mailbox {
-            name: Some("John Doe".into()),
-            email: "john.doe@server.com".into(),
-        },
-        None,
-    );
-    _mailer.set_footer("Visit our website");
-    let builder = _mailer.template(Template::new(
-        Archetype::PlainHtml {
-            title: "Mail title".to_owned(),
-            body: "Mail body".to_owned(),
-        },
-        Some(Language::English),
-    ));
-    let mail = builder.build();
-    mail.unwrap().send();
-}
+// #[test]
+// fn builder() {
+//     let mut _mailer = Mailer::new(
+//         "smtp.server.com",
+//         "smtp_username",
+//         "smtp_password",
+//         Mailbox {
+//             name: Some("John Doe".into()),
+//             email: "john.doe@server.com".into(),
+//         },
+//         None,
+//     );
+//     _mailer.set_footer("Visit our website");
+//     let builder = _mailer.template(Template::new(
+//         Archetype::PlainHtml {
+//             title: "Mail title".to_owned(),
+//             body: "Mail body".to_owned(),
+//         },
+//         Some(Language::English),
+//     ));
+//     let mail = builder.build();
+//     mail.unwrap().send();
+// }
