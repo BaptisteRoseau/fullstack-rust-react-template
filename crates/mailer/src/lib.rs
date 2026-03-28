75a5081eccaeed7304a9a4170c4f5eaa17619730
Baptiste Roseau <baptiste.roseau@opendatasoft.com>
2026-03-28 20:21:12 +0100

    Comment out unused mailer

--- crates/mailer/src/lib.rs
+++ crates/mailer/src/lib.rs
@@ -1,9 +1,9 @@
-mod builder;
-mod error;
-mod templates;
+// mod builder;
+// mod error;
+// mod templates;
 
-pub use builder::{Email, EmailBuilder, Mailer};
-pub use error::Error;
-pub use templates::{Archetype, Language, Template};
+// pub use builder::{Email, EmailBuilder, Mailer};
+// pub use error::Error;
+// pub use templates::{Archetype, Language, Template};
 
-pub use lettre::message::Mailbox;
+// pub use lettre::message::Mailbox;
