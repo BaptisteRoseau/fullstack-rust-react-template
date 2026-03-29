7f169230cde6a06aeb95b9c56171563a2967902d
Baptiste Roseau <baptiste.roseau@opendatasoft.com>
2026-03-29 19:33:23 +0200

    Implement missing trait

--- crates/app_core/src/error.rs
+++ crates/app_core/src/error.rs
@@ -5,3 +5,9 @@ pub enum CoreError {
     #[error("Could not find {0}")]
     NotFound(String),
 }
+
+impl From<Box<database::error::DatabaseError>> for Box<CoreError> {
+    fn from(value: Box<database::error::DatabaseError>) -> Self {
+        Box::new(CoreError::DatabaseError(value))
+    }
+}
