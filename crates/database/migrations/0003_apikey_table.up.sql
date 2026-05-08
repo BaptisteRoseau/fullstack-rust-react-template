b46f18ee4c1e392532cb429e494674c6914d46ee
Baptiste Roseau <baptiste.roseau@opendatasoft.com>
2026-05-08 16:06:43 +0200

    Add index on api key owner

--- crates/database/migrations/0003_apikey_table.up.sql
+++ crates/database/migrations/0003_apikey_table.up.sql
@@ -1,6 +1,8 @@
-CREATE TABLE IF NOT EXISTS apikey (
+CREATE TABLE IF NOT EXISTS api_key (
     key varchar(256) UNIQUE NOT NULL,
     owner FOREIGN KEY NOT NULL,
     permissions JSON NOT NULL DEFAULT '[]'
     PRIMARY KEY(key)
 );
+
+CREATE INDEX IF NOT EXISTS idx__api_key__owner ON api_key (owner);
