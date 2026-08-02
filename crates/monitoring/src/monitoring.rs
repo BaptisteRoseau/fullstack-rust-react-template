5f6b544d0f04fe7975e0bed8ba67587b56e3bfa6
Baptiste Roseau <broseau@gmail.com>
2026-08-02 16:26:25 +0200

    Simple implementation of the monitoring crate

--- crates/monitoring/src/monitoring.rs
+++ crates/monitoring/src/monitoring.rs
@@ -1,12 +1,18 @@
+use metrics::gauge;
+
+const CONNECTION_UP: u8 = 0;
+const CONNECTION_DOWN: u8 = 1;
+
 pub trait MonitoringConnectionName {
-    fn name() -> &'static str;
+    fn monitoring_connection_name(&self) -> String;
 }
 
 pub trait OnConnection<T: MonitoringConnectionName> {
-    fn on_connection_success(&self) {
-        // Prometheus connection UP
+    fn on_connection_success(item: &T) {
+        gauge!(item.monitoring_connection_name()).set(CONNECTION_UP);
     }
-    fn on_connection_failure(&self) {
-        // Prometheus metric connection DOWN
+
+    fn on_connection_failure(item: &T) {
+        gauge!(item.monitoring_connection_name()).set(CONNECTION_DOWN);
     }
 }
